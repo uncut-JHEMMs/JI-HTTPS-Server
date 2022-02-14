4499f509a986f7e87c0a82fb9b0b3dc023f31edf
Jordan Ibarra <jnibarra02@protonmail.com>
2022-02-14 10:59:54 -0500

    JHEMMS-68: Created simple http server with configuration through cmdline, environment variables, and config files.

---
+++ src/env.cpp
@@ -0,0 +1,73 @@
+#include "env.hpp"
+
+#include <cstdlib>
+#include <algorithm>
+
+bool is_integer(const std::string& s);
+
+namespace env
+{
+    std::optional<std::string> get_string(const char* name)
+    {
+        if (const char* x = std::getenv(name))
+            return std::string{x};
+        else
+            return {};
+    }
+
+    std::string get_string(const char* name, std::string def)
+    {
+        return get_string(name).value_or(def);
+    }
+
+    std::optional<bool> get_bool(const char* name)
+    {
+        if (auto optstr = get_string(name))
+        {
+            auto str = *optstr;
+            if (is_integer(str))
+            {
+                return !!std::stoi(str);
+            }
+
+            std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+            if (str == "true" || str == "on" || str == "yes" || str == "y")
+                return true;
+            else
+                return false;
+        }
+
+        return {};
+    }
+
+    bool get_bool(const char* name, bool def)
+    {
+        return get_bool(name).value_or(def);
+    }
+
+    std::optional<int> get_int(const char* name)
+    {
+        if (auto optstr = get_string(name); optstr && is_integer(*optstr))
+        {
+            return std::stoi(*optstr);
+        }
+
+        return {};
+    }
+
+    int get_int(const char* name, int def)
+    {
+        return get_int(name).value_or(def);
+    }
+}
+
+bool is_integer(const std::string& s)
+{
+    if (s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+')))
+        return false;
+
+    char* p;
+    strtol(s.c_str(), &p, 10);
+
+    return (*p == 0);
+}
